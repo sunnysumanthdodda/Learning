d11368171b8a63ad0d10fdc3341eb008d5c6ea84
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-03-10 04:41:34 -0600

    Constructor concept

--- src/com/learning/constructor/BookRunner.java
+++ src/com/learning/constructor/BookRunner.java
@@ -7,8 +7,10 @@ public class BookRunner {
 
         Book shakespeare = new Book();
 
-        System.out.println("No of copies of J.K.Rolling books are: " + jkrolling.getCopies());
+        Book howtolearncoding = new Book(250);
 
+        System.out.println("No of copies of J.K.Rolling books are: " + jkrolling.getCopies());
         System.out.println("No of copies of William Shakespeare books are: " + shakespeare.getCopies());
+        System.out.println("No of copies of Coding books are: " + howtolearncoding.getCopies());
     }
 }
