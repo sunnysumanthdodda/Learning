729077e1e7f6b6c2872f3b3e4d3baedce6962831
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-03-11 05:56:33 -0600

    Character Data Type Concept

---
+++ src/com/learning/primitivedatatypes/MyChar.java
@@ -0,0 +1,52 @@
+package com.learning.primitivedatatypes;
+
+public class MyChar {
+    private final char ch;
+
+    public char getCh() {
+        return ch;
+    }
+
+    public MyChar(char ch){
+        this.ch = ch;
+    }
+
+    public boolean isVowel() {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
+    public void isConsonant(){
+        if(isVowel()) System.out.println("The character "+getCh()+" is not consonant");
+        else System.out.println("The character "+getCh()+" is consonant");
+    }
+
+    public void isNumber() {
+        if(ch >= 48 && ch <=57) System.out.println("The character "+getCh()+" is number");
+        else System.out.println("The character "+getCh()+" is not number");
+    }
+
+    public void isLowerCase() {
+        if(ch >= 97 && ch <=122) System.out.println("The character "+getCh()+" is lowercase Alphabet");
+        else System.out.println("The character "+getCh()+" is not lowercase Alphabet");
+    }
+
+    public void isUpperCase() {
+        if(ch >= 65 && ch <=90) System.out.println("The character "+getCh()+" is uppercase Alphabet");
+        else System.out.println("The character "+getCh()+" is not uppercase Alphabet");
+    }
+
+    public static void printLowerCaseAlphabets() {
+        for(int i=97;i<=122;i++){
+            char c = (char) i;
+            System.out.print(c+" ");
+        }
+    }
+
+    public static void printUpperCaseAlphabets() {
+        for(int i=65;i<=90;i++){
+            char c = (char) i;
+            System.out.print(c+" ");
+        }
+    }
+}
---
+++ src/com/learning/primitivedatatypes/MyCharRunner.java
@@ -0,0 +1,22 @@
+package com.learning.primitivedatatypes;
+
+import java.util.Scanner;
+
+public class MyCharRunner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the character: ");
+        MyChar character = new MyChar(sc.next().charAt(0));
+        if(character.isVowel()) System.out.println("The character "+character.getCh()+" is vowel");
+        else System.out.println("The character "+character.getCh()+" is not vowel");
+        character.isConsonant();
+        character.isNumber();
+        character.isLowerCase();
+        character.isUpperCase();
+        System.out.print("Lower Case Alphabets are: ");
+        MyChar.printLowerCaseAlphabets();
+        System.out.println();
+        System.out.print("Upper Case Alphabets are: ");
+        MyChar.printUpperCaseAlphabets();
+    }
+}
