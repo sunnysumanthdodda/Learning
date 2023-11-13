79b27f91dae70456f391a4a3c84f3adcedf1cbca
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-11-13 14:37:16 -0600

    Inheritance

--- src/com/learning/inheritance/calculator/VeryAdvCalculator.java
+++ src/com/learning/inheritance/calculator/VeryAdvCalculator.java
@@ -1,7 +1,16 @@
 package com.learning.inheritance.calculator;
 
+
+ class Sample{
+    public static int number(int n){
+        return n;
+    }
+}
 public class VeryAdvCalculator extends AdvCalculator {
+
+    String s1 = "Sunny Sumanth ";
+    String s2 = "Dodda";
     public int pow(int a,int b){
-        return (int)Math.pow(a,b);
+        return (int)Math.pow(a,Sample.number(b));
     }
 }
