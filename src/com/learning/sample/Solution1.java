5eeb8e0d53ef5ee43b295ea0d7e45c22616da3f6
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-11-22 12:34:04 -0600

    Sample

---
+++ src/com/learning/sample/Solution1.java
@@ -0,0 +1,29 @@
+package com.learning.sample;
+
+import java.io.*;
+import java.util.Scanner;
+
+class Result {
+    public static long repeatedString(String s, long n) {
+        long ainstring = 0, ainbalancestring = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a') ainstring++;
+        }
+        for(int i=0;i<(n%s.length());i++){
+            if(s.charAt(i)=='a') ainbalancestring++;
+        }
+        return (ainstring*(n/s.length()))+ainbalancestring;
+    }
+}
+
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        String s = sc.nextLine();
+        System.out.print("Enter the number : ");
+        long num = sc.nextInt();
+
+        System.out.println(Result.repeatedString(s,num));
+    }
+}
---
+++ src/com/learning/sample/Solution2.java
@@ -0,0 +1,35 @@
+package com.learning.sample;
+
+import java.util.Scanner;
+
+class Result1 {
+
+    public static int countingValleys(String path) {
+        // Write your code here
+        int level = 0;  // Current level, starts at sea level
+        int valleyCount = 0;  // Count of valleys
+
+        for (char step : path.toCharArray()) {
+            if (step == 'U') {
+                level++;
+            } else if (step == 'D') {
+                // If going downhill and reaching sea level, count a valley
+                if (level == 0) {
+                    valleyCount++;
+                }
+                level--;
+            }
+        }
+        return valleyCount;
+    }
+}
+public class Solution2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        String s = sc.nextLine();
+
+        System.out.println(Result1.countingValleys(s));
+    }
+}
