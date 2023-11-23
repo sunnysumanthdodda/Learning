8927a005045b68a7d698f9e31caa5bc7a74e05cc
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-11-23 17:28:47 -0600

    Threads

---
+++ src/com/learning/threads/Demo.java
@@ -0,0 +1,26 @@
+package com.learning.threads;
+
+class A extends Thread{
+    public void run(){
+        for(int i=0;i<10;i++) {
+            System.out.println("Hi");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=0;i<10;i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+public class Demo {
+    public static void main(String[] args) {
+
+        A obj1 = new A();
+        B obj2 = new B();
+        obj1.start();
+        obj2.start();
+    }
+}
