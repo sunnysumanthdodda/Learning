d991761588a57233c74c49125b784ad2dd7122d4
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-12-07 10:27:47 -0600

    HackerRank

--- src/com/learning/hackerrank/TaumBday.java
+++ src/com/learning/hackerrank/TaumBday.java
@@ -1,8 +1,14 @@
 package com.learning.hackerrank;
 
+import java.math.BigInteger;
+
 public class TaumBday {
     public static void main(String[] args) {
-        int b=3,  w=5,  bc=3,  wc=4,  z=1;
-        System.out.println(b+w+bc+wc+z);
+        int b=95677,  w=39394,  bc=86983,  wc=311224,  z=588538;
+        BigInteger minCost;
+        if(bc>wc+z) minCost = BigInteger.valueOf((long) b *(wc+z)).add(BigInteger.valueOf((long) w *wc));
+        else if(wc>bc+z) minCost = BigInteger.valueOf((long) b *bc).add(BigInteger.valueOf((long) w *(bc+z)));
+        else minCost = BigInteger.valueOf((long) b *bc).add(BigInteger.valueOf((long) w *wc));
+        System.out.println(minCost.longValue());
     }
 }
