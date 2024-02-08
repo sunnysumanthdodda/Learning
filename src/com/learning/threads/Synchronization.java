bf9f1e9bb364f4f0ea2094d1eb34ff373fd508c7
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2024-02-08 14:03:32 -0600

    Thread Synchronization

---
+++ src/com/learning/threads/Synchronization.java
@@ -0,0 +1,36 @@
+package com.learning.threads;
+
+class Counter{
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
+public class Synchronization {
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+
+        Runnable obj1 = ()->{
+            for(int i=1;i<=1000;i++){
+                counter.increment();
+            }
+        };
+        Runnable obj2 = ()->{
+            for(int i=1;i<=1000;i++){
+                counter.increment();
+            }
+        };
+
+        Thread thread1 = new Thread(obj1);
+        Thread thread2 = new Thread(obj2);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter.count);
+    }
+}
